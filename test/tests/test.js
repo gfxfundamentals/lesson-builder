9690981c5fb3583fb3f3a632dbe683077ac00079
Gregg Tavares <gman@chromium.org>
2022-09-29 11:31:43 -0700

    Solve CI issues
    3 issues
    
    1. Getting the date in git was wrong
       (but it turns out that's not used on the CI)
    
    2. Creating a date is in the local time zone.
    
       Fixed by using Date.UTC
    
    3. Can't compare images because GPUs are different
    
       Fixed by comparing size only

--- test/tests/test.js
+++ test/tests/test.js
@@ -3,7 +3,7 @@
 const path = require('path');
 const ThumbnailGenerator = require('../../thumbnail');
 const utils = require('../../lib/utils.js');
-const { copyFileSync, mkdirSync } = require('fs');
+const { copyFileSync, mkdirSync, statSync, readdirSync } = require('fs');
 const MockDate = require('./mock-date.js');
 
 //const notIt = _ => _;
@@ -106,7 +106,8 @@ describe('test lesson-builder', () => {
     this.timeout(20000);
 
     const buildStuff = require('../../build');
-    const outDir = process.env.UPDATE_EXPECTED ? 'test/expected' : 'out';
+    const updateExpected = !!process.env.UPDATE_EXPECTED;
+    const outDir = updateExpected ? 'test/expected' : 'out';
     const buildSettings = {
       outDir,
       baseUrl: 'https://lesson-builder-test.org',
@@ -118,7 +119,7 @@ describe('test lesson-builder', () => {
       templatePath: 'test/templates',
       owner: 'gfxfundamentals',
       repo: 'lesson-builder',
-      feedDate: new Date(2021, 11, 26),
+      feedDate: new Date(Date.UTC(2021, 11, 26, 8)),
       thumbnailOptions: {
         backgroundFilename: path.join(__dirname, '..', 'lessons', 'resources', 'lesson-builder.png'),
         fonts: [
@@ -146,8 +147,27 @@ describe('test lesson-builder', () => {
       globalThis.Date = oldDate;
     }
 
+    // copy the style file as it's helpful to visualize the results.
     mkdirSync(path.join(outDir, 'test/lessons/resources'), {recursive: true});
     copyFileSync('test/lessons/resources/lesson.css', path.join(outDir, 'test/lessons/resources/lesson.css'));
+
+    // compare the .png file since it's created via canvas and the results can be different depending on GPU
+    // then copy the source so they are not caught in the diffTree
+    {
+      const expectedFiles = readdirSync(path.join('test/expected/test/lessons/screenshots'))
+          .filter(f => f.endsWith('.png') || f.endsWith('.jpg'));
+      for (const filename of expectedFiles) {
+        const expectedFilename = path.join('test/expected/test/lessons/screenshots', filename);
+        const actualFilename = path.join(outDir, 'test/lessons/screenshots', filename);
+        const expectedStat = statSync(expectedFilename);
+        const actualStat = statSync(actualFilename);
+        const sizeRatio = expectedStat.size / actualStat.size;
+        assert(sizeRatio >= 0.8 && sizeRatio <= 1.2, `${actualFilename} size (${actualStat.size}) is not within 20% of ${expectedFilename} size (${expectedStat.size})`);
+        if (!updateExpected) {
+          copyFileSync(expectedFilename, actualFilename);
+        }
+      }
+    }
 
     const diffs = await diffTree('test/expected', outDir);
     if (diffs.exitCode !== 0 || diffs.stderr.length !== 0 || diffs.stdout.length !== 0) {
